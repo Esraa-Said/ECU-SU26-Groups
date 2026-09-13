5b23c5a648a1f75a91fc8a3b487f6db080b002f7
m0hamed_3del <mohamedadel18122005@gmail.com>
2026-09-13 21:13:34 +0300

    Add files via upload

---
+++ session-16/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+const PORT = 3000;
+
+connectDB();
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
